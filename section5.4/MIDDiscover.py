7974e103c3510d1fe8b965e4d6d7516824f883bf
agithuber2023 <145901551+agithuber2023@users.noreply.github.com>
2024-03-02 17:51:45 +0800

    Update MIDDiscover.py

--- section5.4/MIDDiscover.py
+++ section5.4/MIDDiscover.py
@@ -1,25 +1,4 @@
-'''
-Implementation of the Functional Dependency Canonical Cover Miner TANE as described in:
-[1] Hutala et al. - TANE: An Efficient Algorithm for Discovering Functional Approximate Dependencies. 1999.
-
-Copyright (C) 2018 Victor Codocedo <firstname.lastname@gmail.com>
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-'''
-
 import sys
-# sys.path.append('c:/users/lu_mi/appdata/local/programs/python/python37/lib/site-packages')
 import time
 from fca.defs.patterns.hypergraphs import TrimmedPartitionPattern
 from fca.io.transformers import List2PartitionsTransformer
